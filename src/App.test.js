339ddfefd8882697df9dc3bd6ea0e2e55f8ced55
Alan Puglisi <alanpuglisi@Alans-MacBook-Pro.local>
2020-09-16 09:35:30 -0500

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders renders without crashing', () => {
+  render(<MemoryRouter>
+    <App />
+  </MemoryRouter>);
+
 });
