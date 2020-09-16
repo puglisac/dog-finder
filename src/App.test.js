f69940b4a0d65c873c73678025cc887a6cd393e0
Alan Puglisi <alanpuglisi@Alans-MacBook-Pro.local>
2020-09-16 09:51:54 -0500

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -9,3 +9,21 @@ test('renders renders without crashing', () => {
   </MemoryRouter>);
 
 });
+
+test('list off dogs appears', () => {
+  const {getAllByText} = render(<MemoryRouter>
+    <App />
+  </MemoryRouter>);
+
+  expect(getAllByText("Whiskey")[0]).toBeInTheDocument();
+});
+
+test('clicking on dog name renders dog details', ()=> {
+  const {getAllByText, getByText, queryByText} = render(<MemoryRouter>
+    <App />
+  </MemoryRouter>);
+  expect(queryByText("Whiskey loves eating popcorn.")).not.toBeInTheDocument();
+  const link = getAllByText("Whiskey")[0];
+  fireEvent.click(link);
+  expect(getByText("Whiskey loves eating popcorn.")).toBeInTheDocument();
+})
